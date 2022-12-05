e7b0cf1fa8f9168fde29233637bedb55cd7d4b64
shunakid <shunakid@icloud.com>
2022-12-05 22:47:31 +0900

    仮で使用するサイトロゴを変更

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -1,6 +1,13 @@
+import { Flex, Heading, Image, Spacer } from "@chakra-ui/react";
 import React from "react";
 
 const Navbar: React.FC = () => {
-  return <div>Navbar</div>;
+  return (
+    <Flex bg="white" height="60px" p="6px 12px">
+      <Flex align="center">
+        <Image src="/images/logo.png" height="80px" />
+      </Flex>
+    </Flex>
+  );
 };
 export default Navbar;
